c417df89c49bc01958e9c278e5a5df7cf0773d1f
Anutas <92079514+Anutas@users.noreply.github.com>
2022-07-01 17:07:38 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,12 +1,12 @@
 ({
-    name: "SHT30", // Category Name
-    description: "Humidity and Temperature Sensor",
-    author: "IOXhop.com",
+    name: "ZX-Ir Temp", // Category Name
+    description: "Temperature Sensor",
+    author: "Anutas",
     category: "Sensors",
     version: "1.0.0",
     icon: "/static/icon.png", // Category icon
     color: "#8b507c", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
-        "sht30_read"
+        "zx-ir temp_read"
     ]
 });
