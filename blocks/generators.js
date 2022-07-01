22f2d757e4f5ec6a4854d269a60bd27de256aaf2
Anutas <92079514+Anutas@users.noreply.github.com>
2022-07-01 17:12:50 +0700

    Update generators.js

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,9 +1,9 @@
-Blockly.Python['sht30_read'] = function (block) {
-  Blockly.Python.definitions_['import_SHT30'] = 'import SHT30';
+Blockly.Python['ZX-Ir Temp_read'] = function (block) {
+  Blockly.Python.definitions_['import_AMG8833'] = 'import AMG8833';
 
   var dropdown_addr = block.getFieldValue('addr');
   var dropdown_type = block.getFieldValue('type');
 
-  var code = `SHT30.read(${dropdown_addr})[${dropdown_type}]`;
+  var code = `ZX-Ir Temp.read(${dropdown_addr})[${dropdown_type}]`;
   return [code, Blockly.Python.ORDER_NONE];
 };
